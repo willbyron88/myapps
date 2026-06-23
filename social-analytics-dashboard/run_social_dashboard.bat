b848e0890d2ec42dcd78ca56aa385c97492d9fb1
Will Power <willpowerprotocols@users.noreply.github.com>
2026-06-23 12:46:28 -0400

    Move social-analytics-dashboard to willpowerprotocols/wpp_analytics_dashboard

--- social-analytics-dashboard/run_social_dashboard.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Users\willb\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\willb\myapps\social-analytics-dashboard\social_dashboard.py"
-if errorlevel 1 (
-    echo.
-    echo Script exited with an error. Press any key to close...
-    pause >nul
-)
